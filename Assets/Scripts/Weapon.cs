9f4389a48fd645f42c0b6e0331914294fc103f0e
dolks <dylanoelkers165@gmail.com>
2022-07-07 00:14:48 -0400

    added hit VFX when shooting something

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 50;
     [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] GameObject hitVFX;
 
     void Update()
     {
@@ -20,8 +22,15 @@ public class Weapon : MonoBehaviour
         muzzleFlash.Play();
         RaycastHit hit;
         if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range)) {
+            CreateHitImpact(hit);
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
             if (target) {target.TakeDamage(damage);}
         }
+    }
+
+    private void CreateHitImpact(RaycastHit hit)
+    {
+        GameObject newHitVFX = Instantiate(hitVFX, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(newHitVFX, 0.1f);
     }
 }
