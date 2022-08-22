98211ddd72ceee05691912f94157c0c4f6ef4a77
dolks <dylanoelkers165@gmail.com>
2022-08-21 23:35:20 -0400

    added game over canvas, updated project to new unity version, updated skybox

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSession : MonoBehaviour
+{
+    public void LoadScene(int index) {
+        SceneManager.LoadScene(index);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
